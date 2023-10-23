5e035233f6f8bc12c9df91c8de1444a1243e47cf
electromedicina <electromedicina@hospitalnotti.mendoza.gov.ar>
2023-10-23 18:39:49 -0300

    tercer commit

--- http.js
+++ http.js
@@ -1,8 +1,23 @@
 const http = require('http')
+const { createSecurePair } = require('tls')
 
-http.createServer(function(request,response){
-    response.write('Hello world')
-    response.end()
-}).listen(3000)
+const server = http.createServer(function(request,response){
+    console.log(request.url)
+    if(request.url ===  '/')
+        {
+            response.write('welcome to the server')
+            return response.end()
+        }
+    if(request.url === '/about'){
+        response.write('chupala bien puto!')
+        return response.end()}
+    response.write(`<h1> Not found </h1> 
+    <p> Esta página no se encontró </p>
+    <a href='/'> volver a la pagina principal </a>`)
+    return response.end()
+})
+
+server.listen(3000)
 
 console.log('Servidor escuchando en puerto 3000')
+
